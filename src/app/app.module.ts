8125e434dda337d6c20d1eed24e1345c292016da
Andrey Bondarenko <ent101@outlook.com>
2018-02-05 19:44:46 +0200

    add show random cards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
 
 import { AppComponent } from './app.component';
+import { MainAreaComponent } from './main-area/main-area.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-area/main-area.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-area',
+  templateUrl: './main-area.component.html',
+  styleUrls: ['./main-area.component.css']
+})
+export class MainAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i <= this.cardsCount; i++) {
+      this.cardsArray.push(i);
+    }
+    //this.shuffle(this.cardsArray);
+    this.showItemsCount(this.cardsArray);
+  }
+  cardsCount:number = 50;
+  cardsArray:any[] = [];
+  spliceArray:any[] = [];
+  inputText:number = 1;
+
+  shuffle(array:any[]) {
+    let max = array.length;
+    let itemTemp;
+    let item;
+
+    // While there remain elements to shuffle…
+    while (max) {
+      // Pick a remaining element…
+      item = Math.floor(Math.random() * max--);
+      // And swap it with the current element.
+      itemTemp = array[max];
+      array[max] = array[item];
+      array[item] = itemTemp;
+    }
+    this.cardsArray = array;
+  }
+
+  incrimet(){
+    if (this.inputText < this.cardsCount) {
+      this.inputText++;
+    }
+  }
+
+  decrimet(){
+    if (this.inputText > 1) {
+      this.inputText--;
+    }
+  }
+
+  getRandomInt(min=0, max=this.cardsCount) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  showItemsCount(array:any[], num:number = 3){
+    this.spliceArray = [];
+    let item;
+    var tempArray = [];
+    if (num > this.cardsCount) {
+      num = this.cardsCount;
+      this.inputText = this.cardsCount;
+    }
+    if (num <= 0) {
+      num = 1;
+      this.inputText = 1;
+    }
+    this.shuffle(array)
+    for (let i = 0; i < num; i++) {
+      item = this.getRandomInt();
+      if (tempArray.indexOf(item) == -1) {
+        tempArray.push(item);
+        this.spliceArray.push(array[item]);
+      } else {
+        i--;
+      }
+    }
+  }
+}
